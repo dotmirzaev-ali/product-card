c41d1d36a0e53b0698fb28864f82b0966a844ca3
Али Дотмирзаев <ali@ip-10-56-238-107.eu-central-1.compute.internal>
2025-12-02 14:00:57 +0100

    fix bags

--- homework-5.js
+++ homework-5.js
@@ -1,17 +1,16 @@
 // Задание 3.Создаю функцию и применяю интерполяцию
 
-function cityWeather(city, temperature) {
+function takeInfo(city, temperature) {
   console.log(`Сейчас в ${city} температура ${temperature} градусов по Цельсию`);
 }
 
-cityWeather('Москве', -10)
+takeInfo('Москве', -10)
 
 // Задание 4. Создаем функцию, проводим проверку
 
 const SOUND_SPEED = 1225
-const speed = 1220
 
-function getSpeed() {
+function showSpeed() {
   if (speed > SOUND_SPEED) {
     console.log('Сверхзвуковая скорость')
   } else if (speed === SOUND_SPEED) {
@@ -21,28 +20,28 @@ function getSpeed() {
   }
 }
 
-getSpeed()
-
+showSpeed(1220);
+showSpeed(1300);
 // Задание 5. Создаем переменные, с помощью функции выводим сумму
 
 const product = 'Телевизор Samsung';
 const price = 40500
 
-function conclusion(budget) {
-  const billing = budget - price
+function makePurchase(budget) {
   if (budget >= price) {
     console.log(`${product} приобретен. Спасибо за покупку!`)
   } else {
-    console.log(`Вам не хватает ${billing}, пополните баланс!`)
+    const missingAmount = budget - price
+    console.log(`Вам не хватает ${missingAmount}, пополните баланс!`)
   }
 }
 
-conclusion(45000)
-conclusion(10000)
+makePurchase(45000)
+makePurchase(10000)
 
 // Задание 6-7. Дополнительная функция и переменные
 
-function yourAbilities(speed) {
+function compareAbilities(speed) {
   if (speed >= 42) {
     console.log("Ты Усейн Болта пахан что ли?");
   } else {
@@ -50,7 +49,7 @@ function yourAbilities(speed) {
   }
 }
 
-yourAbilities(44);
+compareAbilities(44);
 
 const time = 10:34;
 const yourCountry = "Тюмень"
